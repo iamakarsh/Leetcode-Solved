988bf5286bdb379b4381588a7863b3b70b23e4ec
Akarsh Pratap Singh <144613628+iamakarsh@users.noreply.github.com>
2026-06-01 11:33:42 +0530

    Time: 1 ms (99.89%) | Memory: 45.7 MB (33.05%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,19 +1,13 @@
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public int solve(int row, int col, List<List<Integer>> triangle, int[][] dp) {
         int n = triangle.size();        
         if (row == n - 1) return triangle.get(row).get(col);
-        
-        // Check against Integer.MIN_VALUE instead of -1
         if (dp[row][col] != Integer.MIN_VALUE) return dp[row][col];
         
         int left = solve(row + 1, col, triangle, dp);
         int right = solve(row + 1, col + 1, triangle, dp);
         return dp[row][col] = triangle.get(row).get(col) + Math.min(left, right);
     }
-
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int[][] dp = new int[n][n];
